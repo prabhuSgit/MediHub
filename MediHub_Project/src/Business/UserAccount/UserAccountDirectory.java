62b0ae6a3776ff921af59f06b73f701b286bc7aa
prabhuSubramanian <subramanian.pr@husky.neu.edu>
2018-12-01 11:27:22 -0500

    Fixed errors in Prabhu_P

--- MediHub_Project/src/Business/UserAccount/UserAccountDirectory.java
+++ MediHub_Project/src/Business/UserAccount/UserAccountDirectory.java
@@ -29,8 +29,7 @@ public class UserAccountDirectory {
         for (UserAccount ua : userAccountList) {
             if (ua.getUsername().equals(username)
                     && ua.getPassword().equals(password)
-//                    && ua.getStatus().equals("Active")) {
-                    ) {
+                    && ua.getStatus().equals("Active")) {
                 return ua;
             }
         }
@@ -39,16 +38,7 @@ public class UserAccountDirectory {
 
     public UserAccount createEmployeeAccount(String username, String password, Employee employee, Role role) {
 
-//        if (username.equals("sysadmin")) {
-//            UserAccount userAccount = new UserAccount();
-//            userAccount.setUsername(username);
-//            userAccount.setPassword(password);
-//            userAccount.setEmployee(employee);
-//            userAccount.setRole(role);
-//            userAccount.setStatus("Active");
-//            userAccountList.add(userAccount);
-//            return userAccount;
-//        } else {
+        if (username.equals("sysadmin")) {
             UserAccount userAccount = new UserAccount();
             userAccount.setUsername(username);
             userAccount.setPassword(password);
@@ -57,7 +47,16 @@ public class UserAccountDirectory {
             userAccount.setStatus("Active");
             userAccountList.add(userAccount);
             return userAccount;
-//        }
+        } else {
+            UserAccount userAccount = new UserAccount();
+            userAccount.setUsername(username);
+            userAccount.setPassword(password);
+            userAccount.setEmployee(employee);
+            userAccount.setRole(role);
+            userAccount.setStatus("Pending");
+            userAccountList.add(userAccount);
+            return userAccount;
+        }
 
     }
 
@@ -67,6 +66,7 @@ public class UserAccountDirectory {
         userAccount.setPassword(password);
         userAccount.setCustomer(customer);
         userAccount.setRole(role);
+        userAccount.setStatus("Active");
         userAccountList.add(userAccount);
         return userAccount;
     }
