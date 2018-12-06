2431f176ba7bed0de5861a2b3216d798e8682966
Urja96 <jain.u@husky.neu.edu>
2018-12-05 20:24:41 -0500

    trial push again

--- MediHub_Project/src/Business/Role/AdminRole.java
+++ MediHub_Project/src/Business/Role/AdminRole.java
@@ -11,22 +11,15 @@ import Business.UserAccount.UserAccount;
 import Business.UserAccount.UserAccountDirectory;
 import userinterface.ProviderAdminRole.ProviderWorkAreaJPanel;
 import javax.swing.JPanel;
-import userinterface.MedicalSchoolAdminRole.MedicalSchoolWorkAreaJPanel;
 
 /**
  *
  * @author raunak
  */
 public class AdminRole extends Role{
-    
-    @Override
-   
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, UserAccountDirectory directory) {
-       if (account.equals(enterprise.getEnterpriseType().HealthCareProvider)){ 
-           return new ProviderWorkAreaJPanel(userProcessContainer, enterprise);
-       }
-       else if(account.equals(enterprise.getEnterpriseType().MedicalSchool)){
-            return new MedicalSchoolWorkAreaJPanel(userProcessContainer, enterprise);
-       }
-    
 
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, UserAccountDirectory directory) {
+        return new ProviderWorkAreaJPanel(userProcessContainer, enterprise);
+    }
+}
